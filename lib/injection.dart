c640500c5d4b52aa9569bc0053369b1ded30a0b8
huzaifaedhi22 <96188217+huzaifaedhi22@users.noreply.github.com>
2023-10-10 00:45:47 +0400

    Update injection.dart

--- lib/injection.dart
+++ lib/injection.dart
@@ -3,10 +3,11 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:social_media_app/data/repository/auth_repo.dart';
 import 'package:social_media_app/data/repository/db_queries.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final sl = GetIt.instance;
 
-HttpLink httpLink = HttpLink('https://qumuwpsrvxkplnxfptvc.supabase.co/graphql/v1', defaultHeaders: {'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1bXV3cHNydnhrcGxueGZwdHZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY2MDA1MjcsImV4cCI6MjAxMjE3NjUyN30.Pn6Gtxt3FAo4InycE7-UWdUbea4x6u9cuM1uslJr24Q', 'content-type': 'application/json'});
+HttpLink httpLink = HttpLink(dotenv.env['graphQlUrl'] ?? '', defaultHeaders: {'apikey': dotenv.env['anonkey'] ?? '', 'content-type': 'application/json'});
 
 final GraphQLClient client = GraphQLClient(link: httpLink, cache: GraphQLCache());
 
